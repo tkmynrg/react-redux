de6ee2fecce7a396c31801e21deb23c1e5db3a36
tkmynrg <dlsenok@mail.ru>
2023-09-01 23:03:38 +0300

    init redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <>
-
-    </>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
